f3820bd68ccc7d70a27ab7a61221bf3824ce042e
stopachka <stepan.p@gmail.com>
2016-09-27 20:07:01 -0700

    sm

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ app.use(bodyParser.json())
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
+  res.header(
+    'Access-Control-Allow-Headers',
+    req.headers['Access-Control-Request-Headers']
+  );
   next();
 });
 
